bba8d714a196c9b1ae7bafd46da08c34baa4f7c4
Yani0029 <yani@bellcom.dk>
2014-07-25 15:36:26 +0200

    #53 invoice template changes

--- templates/node--itchefer_invoice.tpl.php
+++ templates/node--itchefer_invoice.tpl.php
@@ -4,97 +4,11 @@
  * @file
  * Default theme implementation to display a node.
  *
- * Available variables:
- * - $title: the (sanitized) title of the node.
- * - $content: An array of node items. Use render($content) to print them all,
- *   or print a subset such as render($content['field_example']). Use
- *   hide($content['field_example']) to temporarily suppress the printing of a
- *   given element.
- * - $user_picture: The node author's picture from user-picture.tpl.php.
- * - $date: Formatted creation date. Preprocess functions can reformat it by
- *   calling format_date() with the desired parameters on the $created variable.
- * - $name: Themed username of node author output from theme_username().
- * - $node_url: Direct URL of the current node.
- * - $display_submitted: Whether submission information should be displayed.
- * - $submitted: Submission information created from $name and $date during
- *   template_preprocess_node().
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. The default values can be one or more of the
- *   following:
- *   - node: The current template type; for example, "theming hook".
- *   - node-[type]: The current node type. For example, if the node is a
- *     "Blog entry" it would result in "node-blog". Note that the machine
- *     name will often be in a short form of the human readable label.
- *   - node-teaser: Nodes in teaser form.
- *   - node-preview: Nodes in preview mode.
- *   The following are controlled through the node publishing options.
- *   - node-promoted: Nodes promoted to the front page.
- *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
- *     listings.
- *   - node-unpublished: Unpublished nodes visible only to administrators.
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- *
- * Other variables:
- * - $node: Full node object. Contains data that may not be safe.
- * - $type: Node type; for example, story, page, blog, etc.
- * - $comment_count: Number of comments attached to the node.
- * - $uid: User ID of the node author.
- * - $created: Time the node was published formatted in Unix timestamp.
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
- * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
- *   teaser listings.
- * - $id: Position of the node. Increments each time it's output.
- *
- * Node status variables:
- * - $view_mode: View mode; for example, "full", "teaser".
- * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
- * - $page: Flag for the full page state.
- * - $promote: Flag for front page promotion state.
- * - $sticky: Flags for sticky post setting.
- * - $status: Flag for published status.
- * - $comment: State of comment settings for the node.
- * - $readmore: Flags true if the teaser content of the node cannot hold the
- *   main body content.
- * - $is_front: Flags true when presented in the front page.
- * - $logged_in: Flags true when the current user is a logged-in member.
- * - $is_admin: Flags true when the current user is an administrator.
- *
- * Field variables: for each field instance attached to the node a corresponding
- * variable is defined; for example, $node->body becomes $body. When needing to
- * access a field's raw values, developers/themers are strongly encouraged to
- * use these variables. Otherwise they will have to explicitly specify the
- * desired field language; for example, $node->body['en'], thus overriding any
- * language negotiation rule that was previously applied.
- *
- * @see template_preprocess()
- * @see template_preprocess_node()
- * @see template_process()
- *
- * @ingroup themeable
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php //print $user_picture; ?>
-
   <?php print render($title_prefix); ?>
-  <?php /*if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; */?>
-  <?php print render($title_suffix); ?>
-
-  <?php /*if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; */?>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php
@@ -103,7 +17,7 @@
       hide($content['links']);
       //print render($content);
       print render($content['print_links']);
-      if (isset($content['field_invoice_company'])) {      
+      if (isset($content['field_invoice_company'])) {
         print render($content['field_invoice_company']);
       }
       if (isset($content['field_invoice_date'])) {
@@ -114,6 +28,26 @@
         }
         print "</div>";
       }
+
+      $dato = $content['field_event_node_id']['#object']->field_event_node_id['und'][0]['entity']->field_arrangement_dato_1;
+      $dato_1 = $dato['und'][0]['value'];
+      $dato_1 = date('d-m-Y',strtotime($dato_1));
+      if ( $dato['und'][0]['value2'] != $dato['und'][0]['value']) {
+        $day2 = 1;
+        $dato_2 = $dato['und'][0]['value2'];
+        $dato_2 = date('d-m-Y',strtotime($dato_2));
+      }
+      else {
+        $day2 = 0;
+      }
+
+      // Signup checked values.
+      $signup_1 = $content['field_node_signup_id']['#object']->field_node_signup_id['und'][0]['entity']->field_signup_days;
+      $field_1 = field_collection_item_load($signup_1['und'][0]['value']);
+      $field_1_checked = $field_1->field_signup_day_2_dato['und'][0]['value'];
+      $signup_2 = $content['field_node_signup_id']['#object']->field_node_signup_id['und'][0]['entity']->field_signup_day_2;
+      $field_2 = field_collection_item_load($signup_2['und'][0]['value']);
+      $field_2_checked = $field_2->field_signup_day_2_dato['und'][0]['value'];
       if (isset($content['field_signup_user'])) {
         print "<div class = 'float-left'>";
         //print render($content['field_signup_user']);
@@ -134,24 +68,31 @@
         if (isset($content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_ean_number['und'])) {
           Print "<br><br>EAN nr. : " . $content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_ean_number['und']['0']['value'];
         }
-        print "</div>";
+        print "<br /><strong>Deltagelse dag(e) - " ;
+        if($field_1_checked && $field_2_checked) {
+          $text = "Begge dage";
+        }
+        elseif ($field_1_checked) {
+          $text = "Dag 1";
+        }
+        elseif ($field_2_checked) {
+          $text = "Dag 2";
+        }
+        print " " . $text;
+        print "</strong></div>";
       }
       
       if (isset($content['field_event_node_id'])) {
         print "<div class = 'arrangementer'>";
 
         print "<strong>Deltagelse i ";
-        print render($content['field_event_node_id']['#object']->field_event_node_id['und'][0]['entity']->title). " den ";
-        $dato = $content['field_event_node_id']['#object']->field_event_node_id['und'][0]['entity']->field_arrangement_dato_1;
-        $dato_1 = $dato['und'][0]['value'];
-        $dato_1 = explode(" ", $dato_1);
-        print $dato_1[0];
-        if ($dato['und'][0]['value2'] != $dato['und'][0]['value']) {
+        print render($content['field_event_node_id']['#object']->field_event_node_id['und'][0]['entity']->title) . " den ";
+        print $dato_1;
+        if ($day2) {
           print " til ";
-          $dato_2 = $dato['und'][0]['value2'];
-          $dato_2 = explode(" ", $dato_2);
-          print $dato_2[0];
+          print $dato_2;
         }
+
         $sted = $content['field_event_node_id']['#object']->field_event_node_id['und'][0]['entity']->location;
 
         if (!empty($sted)) {
@@ -165,20 +106,16 @@
          print "</strong>";
          print "<hr>";
 
-         print " <br>Egenbetaling for deltagelse i seminaret for: <br>";
+         print "Egenbetaling for deltagelse i seminaret for: <br>";
          if (isset($content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_firstname['und'])) {
            print $content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_firstname['und'][0]['value'] ." ";
            if (isset($content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_lastname['und']))
            print $content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_lastname['und'][0]['value'];
          }
-        
 
         print "</div>";
       }
 
-    //  print " <br> Egenbetaling for deltagelse i seminaret for: <br>";
-     // print $content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_firstname['und'][0]['value'] ." ";
-   //   print $content['field_event_node_id']['#object']->field_signup_user['und'][0]['entity']->field_lastname['und'][0]['value'];
       print "<div class='pris'>";
       if (isset($content['field_event_price'])) {
         print "<div class = 'event-price'>";
@@ -211,14 +148,4 @@
 
     ?>
   </div>
-  <?php //print $user_picture; ?>
-  <?php /*if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; */?>
-  <?php //print render($content['links']); ?>
-
-  <?php //print render($content['comments']); ?>
-
 </div>
